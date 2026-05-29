6c13ea2775c18afb377773c106808ea365b2de54
Lukasz Winczura <rondoval@gmail.com>
2026-05-29 18:58:58 +0200

    Remove unused opcode name macros and associated functions from nvme_defs.h

--- nvme.device/include/nvme/nvme_defs.h
+++ nvme.device/include/nvme/nvme_defs.h
@@ -860,26 +860,6 @@ enum nvme_opcode {
 	nvme_cmd_vendor_start	= 0x80,
 };
 
-#define nvme_opcode_name(opcode)	{ opcode, #opcode }
-#define show_nvm_opcode_name(val)				\
-	__print_symbolic(val,					\
-		nvme_opcode_name(nvme_cmd_flush),		\
-		nvme_opcode_name(nvme_cmd_write),		\
-		nvme_opcode_name(nvme_cmd_read),		\
-		nvme_opcode_name(nvme_cmd_write_uncor),		\
-		nvme_opcode_name(nvme_cmd_compare),		\
-		nvme_opcode_name(nvme_cmd_write_zeroes),	\
-		nvme_opcode_name(nvme_cmd_dsm),			\
-		nvme_opcode_name(nvme_cmd_verify),		\
-		nvme_opcode_name(nvme_cmd_resv_register),	\
-		nvme_opcode_name(nvme_cmd_resv_report),		\
-		nvme_opcode_name(nvme_cmd_resv_acquire),	\
-		nvme_opcode_name(nvme_cmd_resv_release),	\
-		nvme_opcode_name(nvme_cmd_zone_mgmt_send),	\
-		nvme_opcode_name(nvme_cmd_zone_mgmt_recv),	\
-		nvme_opcode_name(nvme_cmd_zone_append))
-
-
 
 /*
  * Descriptor subtype - lower 4 bits of nvme_(keyed_)sgl_desc identifier
@@ -1187,37 +1167,6 @@ enum nvme_admin_opcode {
 	nvme_admin_vendor_start		= 0xC0,
 };
 
-#define nvme_admin_opcode_name(opcode)	{ opcode, #opcode }
-#define show_admin_opcode_name(val)					\
-	__print_symbolic(val,						\
-		nvme_admin_opcode_name(nvme_admin_delete_sq),		\
-		nvme_admin_opcode_name(nvme_admin_create_sq),		\
-		nvme_admin_opcode_name(nvme_admin_get_log_page),	\
-		nvme_admin_opcode_name(nvme_admin_delete_cq),		\
-		nvme_admin_opcode_name(nvme_admin_create_cq),		\
-		nvme_admin_opcode_name(nvme_admin_identify),		\
-		nvme_admin_opcode_name(nvme_admin_abort_cmd),		\
-		nvme_admin_opcode_name(nvme_admin_set_features),	\
-		nvme_admin_opcode_name(nvme_admin_get_features),	\
-		nvme_admin_opcode_name(nvme_admin_async_event),		\
-		nvme_admin_opcode_name(nvme_admin_ns_mgmt),		\
-		nvme_admin_opcode_name(nvme_admin_activate_fw),		\
-		nvme_admin_opcode_name(nvme_admin_download_fw),		\
-		nvme_admin_opcode_name(nvme_admin_dev_self_test),	\
-		nvme_admin_opcode_name(nvme_admin_ns_attach),		\
-		nvme_admin_opcode_name(nvme_admin_keep_alive),		\
-		nvme_admin_opcode_name(nvme_admin_directive_send),	\
-		nvme_admin_opcode_name(nvme_admin_directive_recv),	\
-		nvme_admin_opcode_name(nvme_admin_virtual_mgmt),	\
-		nvme_admin_opcode_name(nvme_admin_nvme_mi_send),	\
-		nvme_admin_opcode_name(nvme_admin_nvme_mi_recv),	\
-		nvme_admin_opcode_name(nvme_admin_dbbuf),		\
-		nvme_admin_opcode_name(nvme_admin_format_nvm),		\
-		nvme_admin_opcode_name(nvme_admin_security_send),	\
-		nvme_admin_opcode_name(nvme_admin_security_recv),	\
-		nvme_admin_opcode_name(nvme_admin_sanitize_nvm),	\
-		nvme_admin_opcode_name(nvme_admin_get_lba_status))
-
 enum {
 	NVME_QUEUE_PHYS_CONTIG	= (1 << 0),
 	NVME_CQ_IRQ_ENABLED	= (1 << 1),
@@ -1466,25 +1415,6 @@ enum nvmf_capsule_command {
 	nvme_fabrics_type_auth_send	= 0x05,
 	nvme_fabrics_type_auth_receive	= 0x06,
 };
-
-#define nvme_fabrics_type_name(type)   { type, #type }
-#define show_fabrics_type_name(type)					\
-	__print_symbolic(type,						\
-		nvme_fabrics_type_name(nvme_fabrics_type_property_set),	\
-		nvme_fabrics_type_name(nvme_fabrics_type_connect),	\
-		nvme_fabrics_type_name(nvme_fabrics_type_property_get), \
-		nvme_fabrics_type_name(nvme_fabrics_type_auth_send),	\
-		nvme_fabrics_type_name(nvme_fabrics_type_auth_receive))
-
-/*
- * If not fabrics command, fctype will be ignored.
- */
-#define show_opcode_name(qid, opcode, fctype)			\
-	((opcode) == nvme_fabrics_command ?			\
-	 show_fabrics_type_name(fctype) :			\
-	((qid) ?						\
-	 show_nvm_opcode_name(opcode) :				\
-	 show_admin_opcode_name(opcode)))
 
 struct nvmf_common_command {
 	u8	opcode;
